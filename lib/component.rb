19f78e3299e1350a2c641274fefba4f305c9b463
Garrett Smith <garrettsmith@live.ca>
2012-01-30 23:55:42 -0600

    Removed old rubish

--- lib/component.rb
+++
@@ -1,7 +0,0 @@
-##
-# A component represents a single trait of an Entity.
-class Component
-  def key 
-    self.class
-  end
-end
--- lib/manager.rb
+++ lib/manager.rb
@@ -1,8 +1,4 @@
-# A manager provide a global service to the framework.
+# A manager provides a global service to the framework.
 class Manager
 
-  #Return the symbol of this object's class
-  def key
-    self.class.name.to_sym
-  end
 end
